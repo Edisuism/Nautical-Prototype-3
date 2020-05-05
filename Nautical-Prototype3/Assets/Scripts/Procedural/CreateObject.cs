291dffb2b446c5a954fbf7ec6ba6649ebb5a108e
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-05-05 14:40:31 +1000

    terrain gen

---
+++ Nautical-Prototype3/Assets/Scripts/Procedural/CreateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///////////////////////////////////////////Creates a random tile from a list/////////////////////////////////////////////////////
+//alternatively, could have parts of tiles such as a hill and be called by another script that loops over all pos for features
+public class CreateObject : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    private void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+
+}
