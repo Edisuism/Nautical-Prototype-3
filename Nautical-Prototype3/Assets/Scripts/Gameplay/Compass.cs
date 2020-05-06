2b8d0e469b37b744595dcefd3703400146e88c49
cazzerty <61497672+cazzerty@users.noreply.github.com>
2020-05-06 10:07:51 +1000

    Compass Fix

---
+++ Nautical-Prototype3/Assets/Scripts/Gameplay/Compass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = -1 * GameObject.FindGameObjectWithTag("ShipPivot").GetComponent<ShipMovAcc>().getEulerAngles();
+    }
+}
