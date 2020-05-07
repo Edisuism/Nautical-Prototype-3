8116a702c43793c3655ea23787648c8c4c47f75f
cazzerty <61497672+cazzerty@users.noreply.github.com>
2020-05-08 00:47:27 +1000

    Stuff Works

---
+++ Nautical-Prototype3/Assets/Scripts/Gameplay/AnchorAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorAnim : MonoBehaviour
+{
+    public Sprite[] sprites;
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.onAnchorInteract += setAnchor;
+    }
+
+    // Update is called once per frame
+    void setAnchor()
+    {
+        if(i == 0){
+            gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
+            i = 1;
+        }else{
+            gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        i = 0;
+        }
+        
+    }
+}
