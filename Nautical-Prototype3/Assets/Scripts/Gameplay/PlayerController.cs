cd6fa8cbc83d08d51a055f6085738209e432f88b
cazzerty <61497672+cazzerty@users.noreply.github.com>
2020-05-07 16:32:48 +1000

    Fixed Collision Detection Issue

--- Nautical-Prototype3/Assets/Scripts/Gameplay/PlayerController.cs
+++ Nautical-Prototype3/Assets/Scripts/Gameplay/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed = 2f;
     private Vector2 moveInput;
     //public Rigidbody2D ShipRB2D;
-    private string inRange;
+    public string inRange;
     private int carrying = 0;
     public GameObject mimic;
     Animator anim;
@@ -55,10 +55,9 @@ public class PlayerController : MonoBehaviour
         }else{
             Debug.Log(playerID + " is on the wheel");
             GameObject.FindGameObjectWithTag("ShipPivot").GetComponent<ShipMovAcc>().setInput(moveInput.x * -1);
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
-            transform.position = new Vector3(0,-3,0);
+            transform.position = new Vector2(0,-3);
         }
-        animatorHandler();
+        //animatorHandler();
         
     }
     private void animatorHandler(){
@@ -124,29 +123,38 @@ public class PlayerController : MonoBehaviour
                         }
                     }
                 break;
+                case "Materials":
+                    carrying = 1;
+                break;
             } 
+    }
+    private void OnDrop(){
+        Debug.Log("Drop");
     }
     bool wheelLocked(int x){
         return GameObject.FindGameObjectWithTag("ShipPivot").GetComponent<ShipMovAcc>().getWheelInUse() == x;
     }
     //Store nearest object thats interactable
     void OnTriggerEnter2D(Collider2D dataFromCollision) {
-            inRange = dataFromCollision.gameObject.name;
-            
-            switch(inRange){
-                case "BoatPlayable":
-                    inRange = null;
-                break;
+            if(!colIgnore(dataFromCollision.name)){
+                inRange = dataFromCollision.gameObject.name;
+            }
+                
+    }
+    bool colIgnore(string name){
+        //Make sure collisions was only with interactables
+        switch(name){
+            case "BoatPlayable":
+                return true;
                 case "Pirate(Clone)":
-                    inRange = null;
-                break;
-            } 
+                return true;
+        }
+        return false; 
     }
     //reset when out of range
     void OnTriggerExit2D(Collider2D dataFromCollision) {
         if(!(dataFromCollision.gameObject.name.Equals("BoatPlayable"))){
             inRange = null;
-            Debug.Log(inRange);
         }
     }
 }
