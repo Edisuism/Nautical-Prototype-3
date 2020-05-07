d3490e396e76b2cb40aadb62cada17a1ab62fd05
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-05-08 02:03:29 +1000

    chest

--- Nautical-Prototype3/Assets/Scripts/EndTrigger.cs
+++ Nautical-Prototype3/Assets/Scripts/EndTrigger.cs
@@ -5,16 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class EndTrigger : MonoBehaviour
 {
-    public GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(player);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
